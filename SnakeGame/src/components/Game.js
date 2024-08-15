0456485d24061c8360785634939bf55760371ad8
anglusa <otavio.baroni@etec.sp.gov.br>
2024-08-15 11:41:32 -0300

    terminando o Game.js e o Snake.js

---
+++ SnakeGame/src/components/Game.js
@@ -0,0 +1,162 @@
+
+
+import { Dimensions, SafeAreaView, StyleSheet } from "react-native";
+
+import { Direction } from "../types";
+import * as Haptics from "expo-haptics";
+
+const { height } = Dimensions.get("window");
+import {
+    COLS,
+    FOOD_START,
+    HEADER_HEIGHT,
+    INCREMENT,
+    PIXEL,
+    SNAKE_START,
+    SPEED,
+} from "../consts";
+
+import Header from "./Header"
+import Board from "./Board"
+import Snake from "./Snake"
+import Food from "./Food"
+
+import { colors } from "../styles/theme"
+
+const Game = () => {
+    const [Direction, setDirection] = useState(Direction.Right);
+    const [snake, setSnake] = useState(SNAKE_START);
+    const [food, setFood] = useState(FOOD_START);
+    const [isGameOver, setIsGameOver] = useState(false);
+    const [IsGamePaused, setIsGamePaused] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const insets = useSafeAreaInsets();
+    const ROWS = Math.floor(
+        (height - insets?.top - insets?.bottom - HEADER_HEIGHT) / PIXEL
+    );
+    const limits = {
+        minX: 0,
+        maxX: COLS - 1,
+        minY: 0,
+        maxY: ROWS - 1,
+    };
+
+    function resetGame() {
+        setSnake(SNAKE_START);
+        setDirection(Direction.Right);
+    }
+
+    useEffect(() => {
+        if (!isGameOver) {
+            const speedInterval = setInterval(() => {
+                !IsGamePaused && moveSnake();
+            }, SPEED);
+            return () => clearInterval(speedInterval);
+        } else {
+            resetGame();
+        }
+    }, [snake, isGameOver, IsGamePaused]);
+
+    function handleGesture(event) {
+        const { translationX, translationY } = event.nativeEvent;
+
+        if (Math.abs(translationX) > Math.abs(translationY)) {
+            if (translationX > 0) {
+                setDirection(Direction.Right)
+            } else {
+                setDirection(Direction.Left)
+            }
+        } else {
+            if (translationY > 0) {
+                setDirection(Direction.Down)
+            } else {
+                setDirection(Direction.up)
+            }
+        }
+    }
+
+    function moveSnake() {
+        const head = { ...snake[0] };
+
+        switch (Direction) {
+            case Direction.Right:
+                head.x += 1;
+                break;
+            case Direction.Left:
+                head.x -= 1;
+                break;
+            case Direction.Down:
+                head.y += 1;
+                break;
+            case Direction.Up:
+                head.y -= 1;
+                break;
+            default:
+                break;
+        }
+        if (testGameOver(head)) {
+            setIsGameOver(true);
+            Haptics.notificationsAsync(Haptics.NotificationsFeedackType.Error);
+            return;
+        }
+        if (testEatsFood(head, food)) {
+            Haptics.notificationsAsync(Haptics.NotificationsFeedackType.Success);
+            setFood(newFoodPosition(limits));
+            setSnake([head, ...snake]);
+            setScore((prevscore) => prevScore + INCREMENT);
+        } else {
+            setSnake([head, ...snake.slice(0, -1)]);
+        }
+    }
+}
+
+function testGameOver(snakeHead) {
+    return (
+        snakeHead.x < limits.mixX ||
+        snakeHead.x > limits.maxX ||
+        snakeHead.x < limits.minY ||
+        snakeHead.x > limits.maxY
+    );
+}
+
+function testEatsFood(snakeHead, foodLocation) {
+    return snakeHead.x == foodLocation.x && snakeHead.y == foodLocation.y;
+}
+
+function newFoodPosition() {
+    return {
+        x: Math.floor(Math.random() * limits.maxX),
+        y: Math.floor(Math.random() * limits.maxY),
+    }
+};
+
+const RamdomFood = useMemo(() => {
+    return <Food coords={{ x: food.x, y: food.y }} top={insets.top} />;
+}, [food]);
+
+return (
+    <PanGestureHandler onGestureEvent={handleGesture}>
+        <SafeAreaView style={StyleSheet.container}>
+            <Header
+                top={insets.top}
+                score={score}
+                paused={IsGamePaused}
+                pause={() => setIsGamePaused((prev) => !prev)}
+                reload={() => setIsGameOver((prev) => !prev)}
+            />
+            <Board rows={ROWS} cols={COLS} top={insets.top} />
+            <Snake snake={snake} top={insets.top} />
+            {RamdomFood}
+        </SafeAreaView>
+    </PanGestureHandler>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.p6,
+        flex: 1,
+    },
+})
+
+export default Game
